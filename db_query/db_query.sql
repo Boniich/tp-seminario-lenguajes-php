16a59cbc257149ada466adcb8f0268ed864387cc
boniich <ezequieldbo25@gmail.com>
2023-08-29 18:30:32 -0300

    feat: add folder that contains db scripts

---
+++ db_query/db_query.sql
@@ -0,0 +1,16 @@
+use db_seminario_lenguajes_php;
+
+
+/*
+CREATE TABLE users (
+        id int NOT NULL AUTO_INCREMENT,
+        email varchar(128) NOT NULL,
+        password varchar(128) NOT NULL,
+        PRIMARY KEY (id)
+);*/
+
+/*
+INSERT INTO users (email, password)
+VALUES ("ezequiel@gmail.com", "123456");*/
+
+select * from users;
