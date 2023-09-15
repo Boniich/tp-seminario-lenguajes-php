3c91b4b58ab3ccc69abc1821f5a9a3862b629bd6
boniich <ezequieldbo25@gmail.com>
2023-09-15 11:56:01 -0300

    feat: create a file to show msg when a product is created successfully

---
+++ application/views/admin/products_created_successfully_msg.php
@@ -0,0 +1,24 @@
+<?php
+defined('BASEPATH') or exit('No direct script access allowed');
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <title>Admin Panel</title>
+
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+</head>
+
+<body>
+    <div class="container">
+        <div class="my-5">
+            <h1 class="text-center">Product created successfully</h1>
+            <td><a href="<?php echo site_url("admin_panel"); ?>">Go to Admin Panel</a></td>
+            <td><a href="<?php echo site_url("products"); ?>">Go to products list</a></td>
+        </div>
+</body>
+
+</html>
