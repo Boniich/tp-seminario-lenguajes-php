7d3d3f4b521502dd61baa071e6a491866e4963b7
boniich <ezequieldbo25@gmail.com>
2023-09-15 10:48:02 -0300

    feat: add in an indidual file products list for normal user

---
+++ application/views/products/show_products_list.php
@@ -0,0 +1,34 @@
+<?php
+defined('BASEPATH') or exit('No direct script access allowed');
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <title>Products</title>
+
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+</head>
+
+<body>
+    <div class="row">
+        <?php foreach ($products as $product) : ?>
+            <div class="col mb-5">
+                <div class="card" style="width: 18rem;">
+                    <img src="<?php echo base_url($product['image']); ?>" class="card-img-top" alt="...">
+                    <div class="card-body">
+                        <h5 class="card-title"><?php echo $product['name']; ?></h5>
+                        <p class="card-text"><?php echo $product['description']; ?></p>
+                        <p class="card-text">$ <?php echo $product['price']; ?></p>
+                        <a href="#" class="btn btn-primary">Add to car</a>
+                    </div>
+                </div>
+            </div>
+        <?php endforeach; ?>
+    </div>
+    </div>
+</body>
+
+</html>
