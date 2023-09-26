bf35a34c6a3c556b05f797f6b7376c34917935ce
boniich <ezequieldbo25@gmail.com>
2023-09-25 22:18:09 -0300

    feat: improve login model to addapt it to sessions

--- application/models/Login_model.php
+++ application/models/Login_model.php
@@ -1,19 +1,41 @@
 <?php
 class Login_model extends CI_Model
 {
+
+    private int $user_id = 0;
+
     public function __construct()
     {
         $this->load->database();
     }
 
-    public function login($email, $password)
+    private function set_user_id($query)
+    {
+        $user = $query->row();
+        $this->user_id = $user->id;
+    }
+
+    public function get_user_id()
+    {
+        return $this->user_id;
+    }
+
+    private function validate_user_data($email, $password)
     {
         $this->db->where('email', $email);
         $this->db->where('password', $password);
         $query = $this->db->get('users');
 
-        $status = $query->num_rows() == 1 ?? true ?? false;
+        return $query;
+    }
 
+    public function login($email, $password)
+    {
+        $query = $this->validate_user_data($email, $password);
+        $status = $query->num_rows() == 1 ?? true ?? false;
+        if ($status) {
+            $this->set_user_id($query);
+        }
         return $status;
     }
 }
