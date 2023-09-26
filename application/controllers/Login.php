bdbad133a33bd5f4c4a4c67c1c125591830a0442
boniich <ezequieldbo25@gmail.com>
2023-09-25 22:14:52 -0300

    feat: add session feature making posible authenticated a user and make it logout too

--- application/controllers/Login.php
+++ application/controllers/Login.php
@@ -10,24 +10,39 @@ class Login extends CI_Controller
         $this->load->helper('form');
         $this->load->helper('url_helper');
         $this->load->model('login_model');
+        $this->load->library('session');
     }
 
 
     public function index()
     {
+
+        if ($this->session->userdata('user_id')) {
+            redirect('products');
+        }
+
         $this->load->view('login_form');
     }
 
     public function do_login()
     {
+
         $email = $this->input->post('email');
         $password = $this->input->post('password');
 
         if ($this->login_model->login($email, $password)) {
+            $id = $this->login_model->get_user_id();
+            $this->session->set_userdata('user_id', $id);
             redirect('products');
         } else {
             $data['error_message'] = 'Invalid username or password';
             $this->load->view('login_form', $data);
         }
+    }
+
+    public function logout()
+    {
+        $this->session->unset_userdata('user_id');
+        redirect('login');
     }
 }
